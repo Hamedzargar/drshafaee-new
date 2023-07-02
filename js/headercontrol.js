fb9e5db671a7f18473b97755b75011d7c0a2ae27
mahla-mohamadi <137606146+mahla-mohamadi@users.noreply.github.com>
2023-07-02 14:24:28 +0330

    finallyyyy (#5)
    * contact
    
    * about
    
    * ssss
    
    * ee2
    
    * kkkkkkkk
    
    * final
    
    ---------
    
    Co-authored-by: Hamedzargar <70194644+Hamedzargar@users.noreply.github.com>

--- js/headercontrol.js
+++ js/headercontrol.js
@@ -1,6 +1,9 @@
 var $ = jQuery;
 $(document).ready(function(){
     $('.menuSection .mainMenu > ul > .menu-item-has-children > a').append('<svg height="10" width="10" fill="#28416e" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330" xml:space="preserve"><path d="M325.607 79.393c-5.857-5.857-15.355-5.858-21.213.001l-139.39 139.393L25.607 79.393c-5.857-5.857-15.355-5.858-21.213.001-5.858 5.858-5.858 15.355 0 21.213l150.004 150a14.999 14.999 0 0 0 21.212-.001l149.996-150c5.859-5.857 5.859-15.355.001-21.213z"/></svg>');
+    $('.mobileMenyParent').css({
+        'right':'-100px',
+    });
     $('.mobileMenuSection').click(function(){
         $('.mainSiteOverlay').addClass('showSiteOverlay');
         $('.mobileMenyParent').show(30);
@@ -10,10 +13,10 @@ $(document).ready(function(){
     });
     $('.mobileMenuClose').click(function(){
         $('.mainSiteOverlay').removeClass('showSiteOverlay');
+        $('.mobileMenyParent').hide(30);
         $('.mobileMenyParent').css({
             'right':'-100px',
         });
-        $('.mobileMenyParent').hide(30);
     });
     $(window).on('scroll' , function(){
         if($(document).scrollTop() >= 160){
@@ -23,5 +26,21 @@ $(document).ready(function(){
         else{
             $('.stickyHeaderParent').removeClass('showStickyHeader');                        
         }
+    });
+    $('.mainHeader .headericons .searchIcon').click(function(){
+        $('.mainSiteOverlay').addClass('showSiteOverlay');
+        $('.searchContainer').slideDown(450);
+    });
+    $('.searchButtonContainer .closeSearchBtn').click(function(){
+        $('.searchContainer').slideUp(400);
+        $('.mainSiteOverlay').removeClass('showSiteOverlay');
+    });
+    $('.mainSiteOverlay').click(function(){
+        $('.searchContainer').slideUp(400);
+        $('.mobileMenyParent').css({
+            'right':'-100px',
+        });
+        $('.mobileMenyParent').hide(30);
+        $('.mainSiteOverlay').removeClass('showSiteOverlay');
     });
 });
--- template-parts/content.php
+++ template-parts/content.php
@@ -8,9 +8,9 @@
  */
 
 ?>
-
+<div class="generalSinglePostStyle singleBox lowRadius mediumPadding">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header pageCenterHeading">
+	<header class="entry-header generalHeading bigHeading highMargined centeredText bottomBorder">
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
@@ -20,22 +20,21 @@
 
 		if ( 'post' === get_post_type() ) :
 			?>
-			<div class="innerBlogCardHeaderItem">
-				<svg fill="#707070" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 22"><g fill="none" fill-rule="evenodd" stroke="#707070" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" transform="translate(1 1)"><rect width="18" height="18" y="2" rx="2"></rect><path d="M13 0v4M5 0v4M0 8h18"></path></g></svg>
-				<span><?php echo get_the_date() ?></span>
-			</div>
 		<?php endif; ?>
 	</header><!-- .entry-header -->
+	<div class="innerBlogCardHeaderItem highMargined">
+		<div class="blogCardHeaderItem">
+			<svg fill="#28416e" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 22"><g fill="none" fill-rule="evenodd" stroke="#707070" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" transform="translate(1 1)"><rect width="18" height="18" y="2" rx="2"></rect><path d="M13 0v4M5 0v4M0 8h18"></path></g></svg>
+			<span><?php echo get_the_date() ?></span>
+		</div>
+		<div class="blogCardHeaderItem authorPart">
+			<svg  width="20" height="20" fill="#28416e" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" clip-rule="evenodd" viewBox="0 0 32 32"><path d="M11.575 15.664C5.453 17.554 1 23.261 1 30a1 1 0 0 0 2 0c0-7.175 5.825-13 13-13s13 5.825 13 13a1 1 0 0 0 2 0c0-6.739-4.453-12.446-10.575-14.336A7.994 7.994 0 0 0 24 9c0-4.415-3.585-8-8-8S8 4.585 8 9a7.994 7.994 0 0 0 3.575 6.664ZM16 3c3.311 0 6 2.689 6 6s-2.689 6-6 6-6-2.689-6-6 2.689-6 6-6Z"/></svg>
+			<span><?php echo get_the_author() ?></span>
+		</div>
+	</div>
 	<div class="singlePostCard">
 	<?php drshafaee_post_thumbnail(); ?>
 	<div class="blogCardHeader">
-		<div class="blogCardHeaderItem authorPart">
-			<svg  width="25" height="25" fill="#707070" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" clip-rule="evenodd" viewBox="0 0 32 32"><path d="M11.575 15.664C5.453 17.554 1 23.261 1 30a1 1 0 0 0 2 0c0-7.175 5.825-13 13-13s13 5.825 13 13a1 1 0 0 0 2 0c0-6.739-4.453-12.446-10.575-14.336A7.994 7.994 0 0 0 24 9c0-4.415-3.585-8-8-8S8 4.585 8 9a7.994 7.994 0 0 0 3.575 6.664ZM16 3c3.311 0 6 2.689 6 6s-2.689 6-6 6-6-2.689-6-6 2.689-6 6-6Z"/></svg>
-			<span><?php echo get_the_author() ?></span>
-		</div>
-		<div class="blogCardHeaderItem">
-			<?php //get_post_primary_category(); ?>
-		</div>
 	</div>
 	<div class="entry-content">
 		<?php
@@ -54,12 +53,12 @@
 			)
 		);
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'drshafaee' ),
-				'after'  => '</div>',
-			)
-		);
+		// wp_link_pages(
+		// 	array(
+		// 		'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'drshafaee' ),
+		// 		'after'  => '</div>',
+		// 	)
+		// );
 		?>
 	</div><!-- .entry-content -->
 	</div>
@@ -67,3 +66,4 @@
 		<?php //drshafaee_entry_footer(); ?>
 	</footer>.entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
+</div>
