cee0cd432194cf21b2f0a23b0ff8d154dbfcc333
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2023-07-02 20:22:52 +0330

    edit header & create Request-template (#7)
    * footer edit & responsive
    
    * footer final edit
    
    * edit header & create Request-template
    
    ---------
    
    Co-authored-by: Hamedzargar <70194644+Hamedzargar@users.noreply.github.com>

---
+++ page-templates/request-template.php
@@ -0,0 +1,17 @@
+<?php
+/*
+
+Template Name: Request Template
+
+*/
+get_header();
+?>
+
+<?php echo do_shortcode('[contact-form-7 id="29" title="درخواست مشاوره"]') ?>
+
+
+
+
+
+<?php
+get_footer();
